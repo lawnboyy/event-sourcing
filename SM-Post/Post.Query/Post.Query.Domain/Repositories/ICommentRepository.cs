31a4ce99518fc3959c29b2681a250b6a2a788fc8
Alan Hunt <alan.hunt@maplarge.com>
2023-04-09 14:21:29 -0500

    Add same thing as before

---
+++ SM-Post/Post.Query/Post.Query.Domain/Repositories/ICommentRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Post.Query.Domain.Entities;
+
+namespace Post.Query.Domain.Repositories;
+public interface ICommentRepository
+{
+  Task CreateAsync(CommentEntity comment);
+  Task UpdateAsync(CommentEntity comment);
+  Task<CommentEntity> GetByIdAsync(Guid commentId);
+  Task DeleteAsync(Guid commentId);
+}
