97bfcd3f60dae06cbe0d1731ecf07443ca2beac6
Alan Hunt <alan.hunt@maplarge.com>
2023-04-16 09:54:26 -0500

    Implement event handler

--- SM-Post/Post.Query/Post.Query.Domain/Entities/CommentEntity.cs
+++ SM-Post/Post.Query/Post.Query.Domain/Entities/CommentEntity.cs
@@ -9,6 +9,7 @@ public class CommentEntity
 {
   [Key]
   public Guid CommentId { get; set; }
+  public string Comment { get; set; } = "";
   public string Username { get; set; } = "";
   public DateTime CommentDate { get; set; }
   public bool Edited { get; set; }
