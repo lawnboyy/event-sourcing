06afb7e34fbd5cc05a1cd6170e71a28781bff7ae
Alan Hunt <alan.hunt@maplarge.com>
2023-04-16 13:28:21 -0500

    Register class maps for mongo event store

--- SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -11,8 +11,20 @@ using Post.Cmd.Infrastructure.Dispatchers;
 using Confluent.Kafka;
 using CQRS.Core.Producers;
 using Post.Cmd.Infrastructure.Producers;
+using MongoDB.Bson.Serialization;
+using CQRS.Core.Events;
+using Post.Common.Events;
 
 var builder = WebApplication.CreateBuilder(args);
+
+BsonClassMap.RegisterClassMap<BaseEvent>();
+BsonClassMap.RegisterClassMap<PostCreatedEvent>();
+BsonClassMap.RegisterClassMap<MessageUpdatedEvent>();
+BsonClassMap.RegisterClassMap<PostLikedEvent>();
+BsonClassMap.RegisterClassMap<CommentAddedEvent>();
+BsonClassMap.RegisterClassMap<CommentUpdatedEvent>();
+BsonClassMap.RegisterClassMap<CommentRemovedEvent>();
+BsonClassMap.RegisterClassMap<PostRemovedEvent>();
 
 // Add services to the container.
 builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection("MongoDBConfig"));
