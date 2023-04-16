092d2b9777ebacba2f7434ce63e016501cb294f7
Alan Hunt <alan.hunt@maplarge.com>
2023-04-16 09:38:19 -0500

    Implement comment repository

---
+++ SM-Post/Post.Query/Post.Query.Infrastructure/Repositories/CommentRepository.cs
@@ -0,0 +1,59 @@
+using Microsoft.EntityFrameworkCore;
+using Post.Query.Domain.Entities;
+using Post.Query.Domain.Repositories;
+using Post.Query.Infrastructure.DataAccess;
+
+namespace Post.Query.Infrastructure.Repositories;
+
+public class CommentRepository : ICommentRepository
+{
+  private readonly DatabaseContextFactory _contextFactory;
+
+  public CommentRepository(DatabaseContextFactory contextFactory)
+  {
+    _contextFactory = contextFactory;
+  }
+
+  public async Task CreateAsync(CommentEntity comment)
+  {
+    using DatabaseContext context = _contextFactory.CreateDbContext();
+    context.Comments.Add(comment);
+
+    _ = await context.SaveChangesAsync();
+  }
+
+  public async Task DeleteAsync(Guid commentId)
+  {
+    using DatabaseContext context = _contextFactory.CreateDbContext();
+    var comment = await GetByIdAsync(commentId);
+
+    if (comment == null)
+    {
+      return;
+    }
+
+    context.Comments.Remove(comment);
+    _ = await context.SaveChangesAsync();
+  }
+
+  public async Task<CommentEntity> GetByIdAsync(Guid commentId)
+  {
+    using DatabaseContext context = _contextFactory.CreateDbContext();
+    var comment = await context.Comments.FirstOrDefaultAsync(c => c.CommentId == commentId);
+    if (comment == null)
+    {
+      // TODO: Use custom exception.
+      throw new Exception("Comment not found");
+    }
+
+    return comment;
+  }
+
+  public async Task UpdateAsync(CommentEntity comment)
+  {
+    using DatabaseContext context = _contextFactory.CreateDbContext();
+    context.Comments.Update(comment);
+
+    _ = await context.SaveChangesAsync();
+  }
+}
