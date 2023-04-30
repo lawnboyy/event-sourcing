1ddc49fc27712fb225e809f73a82df08b4f05910
Alan Hunt <alan.hunt@maplarge.com>
2023-04-30 16:14:09 -0500

    Add edit comment controller

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/EditCommentController.cs
@@ -0,0 +1,52 @@
+using CQRS.Core.Exceptions;
+using CQRS.Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Post.Cmd.Api.Commands;
+using Post.Common.DTOs;
+
+namespace Post.Cmd.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class EditCommentController : ControllerBase
+{
+  private readonly ILogger<EditCommentController> _logger;
+  private readonly ICommandDispatcher _commandDispatcher;
+
+  public EditCommentController(ILogger<EditCommentController> logger, ICommandDispatcher commandDispatcher)
+  {
+    _logger = logger;
+    _commandDispatcher = commandDispatcher;
+  }
+
+  [HttpPut("{id}")]
+  public async Task<ActionResult> EditCommentAsync(Guid id, [FromBody] EditCommentCommand command)
+  {
+    try
+    {
+      command.Id = id;
+      await _commandDispatcher.SendAsync(command);
+
+      return Ok(new BaseResponse()
+      {
+        Message = "Edit comment request completed successfully!",
+      });
+    }
+    catch (InvalidOperationException ex)
+    {
+      _logger.LogWarning(ex, "Client made a bad request");
+      return BadRequest(new BaseResponse { Message = ex.Message });
+    }
+    catch (AggregateNotFoundException ex)
+    {
+      _logger.LogWarning(ex, "Could not retrieve aggregate; client passed an incorrect post ID targeting the aggregate.");
+      return BadRequest(new BaseResponse { Message = ex.Message });
+    }
+    catch (Exception ex)
+    {
+      const string SAFE_ERROR_MESSAGE = "An error occurred while processing your request to edit a comment on a post!";
+      _logger.LogError(ex, SAFE_ERROR_MESSAGE);
+      return StatusCode(StatusCodes.Status500InternalServerError, new NewPostResponse { Id = id, Message = SAFE_ERROR_MESSAGE });
+    }
+  }
+}
